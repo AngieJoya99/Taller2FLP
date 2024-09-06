f81b82027c86c487d2ad964392c242e1336e5ed3
AngieJoya99 <136751221+AngieJoya99@users.noreply.github.com>
2024-09-06 15:42:51 -0500

    Update ejercicio1.rkt

---
+++ ejercicio1.rkt
@@ -0,0 +1,18 @@
+#|Angie Joya - 2322609
+Emily Nuñez - 2240156|#
+
+#lang eopl
+
+#|Punto 2.1.1: Gramática en listas|#
+
+#|Punto 2.1.2: Gramática en datatypes|#
+
+#|Punto 2.2.1 Parse|#
+
+#|Punto 2.2.2 Unparse|#
+
+#|Punto 2.3.1 Add-edge|#
+
+#|Punto 2.3.2 Vecinos-salientes|#
+
+#|Punto 2.3.3 Vecinos-entrantes|#
