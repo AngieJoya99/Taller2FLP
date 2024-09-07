ad533a95eb8b28a4cfc652062123665813b38db2
AngieJoya99 <angie.joya@correounivalle.edu.co>
2024-09-07 15:54:05 -0500

    Update ejercicio1.rkt

--- ejercicio1.rkt
+++ ejercicio1.rkt
@@ -3,28 +3,67 @@ Emily Nuñez - 2240156|#
 
 #lang eopl
 
-#|Punto 2.1.1: Gramática en listas
-grafo-dirigido: List x List -> List
-usage: (grafo-dirigido v a) = Crea una lista que representa el grafo dirigido
-de vértices v y aristas a 
-
+#|
+-----------------------------------------------------------
+Punto 2.1.1: Gramática en listas
 Gramática:
-<grafo-dirigido> ::= '() | '(graph (vertices <vertice>) (aristas <arista>))
-<vertice> ::= '() | '(<valor-de-scheme> <vertice>)
-<arista> ::= '() | '((<valor-de-scheme> <valor-de-scheme>) <arista>)
+<grafo-dirigido> ::= () | ('graph <vertice> <arista>)
+<vertice> ::= () | ('vertices <valor-de-scheme>+)
+<arista> ::= () | ('aristas (<valor-de-scheme> <valor-de-scheme>)+)
 
-Casos de prueba:
-(grafo-dirigido '(a b c) '((a b) (b c) (c a)))
-(grafo-dirigido '(z x @ w) '((@ x) (x @) (c a) (w z) (w x) (@ z)))
-(grafo-dirigido '(1 6 3 8) '((8 1) (3 6) (6 3) (8 3)))
-(grafo-dirigido '() '())
+vertice: List -> List
+usage: (vertice v) = Crea una lista con la representación de los vertices v:
+la cabeza es la palabra 'vertices y la cola es la lista v
+
+Casos de prueba
+(vertice '(a b c))
+(vertice '(z x @ w))
+(vertice '(1 6 a 8))
+(vertice '())
 |#
 
-(define grafo-dirigido
+(define vertice
+  (lambda (v)
+    (cond
+      [(null? v) '()]
+      [else (list 'vertices v)]
+    )
+  )
+)
+
+#|arista List -> List
+usage: (arista a) = Crea una lista con la representación de las aristas a:
+la cabeza es la palabra 'aristas y la cola es la lista a
+
+Casos de prueba
+(arista '((a b) (b c) (c a)))
+(arista '((@ x) (x @) (c a) (w z) (w x) (@ z)))
+(arista '((8 1) (a 6) (6 a) (8 6)))
+(arista '())|#
+
+(define arista
+  (lambda (a)
+    (cond
+      [(null? a) '()]
+      [else (list 'aristas a)]
+    )
+  )
+)
+
+#|graph: vertice x arista -> List
+usage: (graph v a) = Crea una lista que representa el grafo dirigido de vértices v y aristas a 
+
+Casos de prueba:
+(graph (vertice '(a b c)) (arista '((a b) (b c) (c a))))
+(graph (vertice '(z x @ w)) (arista '((@ x) (x @) (c a) (w z) (w x) (@ z))))
+(graph (vertice '(1 6 a 8)) (arista '((8 1) (a 6) (6 a) (8 6))))
+(graph (vertice '()) (arista '()))|#
+
+(define graph
   (lambda (v a)
     (cond
       [(and (null? v) (null? a)) '()]
-      [else (list 'graph (list 'vertices v) (list 'aristas a))]
+      [else (list 'graph  v a)]
     )
   )
 )
@@ -33,9 +72,22 @@ Casos de prueba:
 
 #|Punto 2.2.1 Parse|#
 
+(define PARSEBNF
+  (lambda (g)
+    ('falta)
+  )
+
+)
+
 #|Punto 2.2.2 Unparse|#
 
 #|Punto 2.3.1 Add-edge|#
+
+(define add-edge
+  (lambda (g a)
+    ('falta)
+  )
+)
 
 #|Punto 2.3.2 Vecinos-salientes|#
 
